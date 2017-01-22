040913ffb22d88d02702daff06b64a5f9067d3da
naveenc131 <naveen.c131@hotmail.com>
2017-01-23 01:02:35 +0530

    added

--- CS50x-Naveen/cat.c
+++ CS50x-Naveen/cat.c
@@ -1,26 +1,60 @@
-Tinclude <string.h>
+#include <stdio.h>
+#include <string.h>
 #include <ctype.h>
+#include <stdbool.h>
 #include <cs50.h>
 int main(int argc, char const *argv[])
 {
-    int counter = 0;
+	int counter = 0;
     char *input = get_string();
     char *token = strtok(input," ");
+    char *prefix = "";
+    int found = 0  ;
     if(input == NULL)
-    {
-       printf("There is no box");
-    }
+        {
+        printf("There is no box");
+        return 1;
+       }
         
 	while(token != NULL)
 	{
 
-		  counter = counter + 1;
+		    counter = counter + 1;
 			if (strcmp(token,"cat") == 0 )
 			{
-				printf("The cat is the %d item in the box",counter);
+                found = found + 1;
+				if(counter % 10 == 1 & counter < 100)
+                {
+                    prefix = "st";
+                    printf("The cat is the %d%s item in the box",counter,prefix);
+                }
+                else if(counter % 10 == 2 && counter < 100)
+                 {
+                    prefix = "nd";
+                    printf("The cat is the %d%s item in the box",counter,prefix);
+                    
+                }
+                 else if(counter % 10 == 3 && counter < 100)
+                 {
+                    prefix = "rd";
+                    printf("The cat is the %d%s item in the box",counter,prefix);
+                    
+                }
+                else if(counter > 100 && counter % 100 < 100)
+                {
+                    prefix = "th";
+                    printf("The cat is the %d%s item in the box",counter,prefix);
+                    
+                }
+               
+           
 			}
-
+       
 		token = strtok(NULL, " ");
 	}
+    if(found == 0)
+        {
+        printf("No cat yet");
+    }
 	return 0;
-} 
+}
