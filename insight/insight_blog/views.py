32b4ea755bf2c5acce40de9cf86ae125281d6a18
naveenc131 <naveen.c131@hotmail.com>
2017-03-28 03:23:32 +0530

    email activation included

--- insight/insight_blog/views.py
+++ insight/insight_blog/views.py
@@ -29,10 +29,41 @@ def register_view(request):
 		user = form.save(commit=False)
 		password = form.cleaned_data.get('password')
 		user.set_password(password)
+		#user.is_active=False
 		user.save()
+		#id=user.id
+		#email=user.email
+		#send_email(email,id)
+		#return render(request,'thankyou.html')
 
 	return render(request,"register.html",{"form":form})
 
+def activate(request):
+	id=int(request.GET.get('id'))
+	user = User.objects.get(id=id)
+	user.is_active=True
+	user.save()
+	return render(request,'activation.html')
+
+
 def logout_view(request):
+	logout(request)
 	return render(request,"form.html",{})
+
+#def send_email(toaddr,id):
+#	text = "Hi!\nHow are you?\nHere is the link to activate your account:\nhttp://127.0.0.1:8000/activation/?id=%s" %(id)
+	# Record the MIME types of both parts - text/plain and text/html.
+#	part1 = MIMEText(text, 'plain')
+#	msg = MIMEMultipart('alternative')
+#	msg.attach(part1)
+#	subject="Activate your account at Family Host"
+#	msg="""\From: %s\nTo: %s\nSubject: %s\n\n%s""" %(fromaddr,toaddr,subject,msg.as_string())
+	#Use gmail's smtp server to send email. However, you need to turn on the setting "lesssecureapps" following this link:
+	#https://www.google.com/settings/security/lesssecureapps
+#	server = smtplib.SMTP('smtp.gmail.com:587')
+#	server.ehlo()
+#	server.starttls()
+#	server.login(username,password)
+#	server.sendmail(fromaddr,[toaddr],msg)
+#	server.quit()
 
