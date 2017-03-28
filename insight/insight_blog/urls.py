3afc966eea7e4bb82a8d8ead9c743e66ec66885d
naveenc131 <naveen.c131@hotmail.com>
2017-03-29 00:00:21 +0530

    urls links

--- insight/insight_blog/urls.py
+++ insight/insight_blog/urls.py
@@ -4,4 +4,6 @@ from . import views
 #Our customised url patterns
 urlpatterns = [ 
 				url(r'^$', views.index, name='index'),
+				url(r'^login/', views.login_view,name="login"),
+    			url(r'^register/', views.register_view,name="register"),
 				]
